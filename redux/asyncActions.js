ee21575b3c8c0f26b3e49b6d643052d2749e1316
Siydulislam <siydulislam333@gmail.com>
2023-03-05 14:38:04 +0600

    asynchronous thunk functions - old way

---
+++ redux/asyncActions.js
@@ -0,0 +1,91 @@
+
+const { default: fetch } = require("node-fetch");
+const { createStore, applyMiddleware } = require("redux");
+const thunkMiddleware = require("redux-thunk");
+
+// initial state
+const initialState = {
+    loading: false,
+    posts: [],
+    error: ""
+};
+
+// action creators
+const fetchPostsRequested = () => {
+    return {
+        type: "posts/requested"
+    }
+}
+
+const fetchPostsSucceeded = (posts) => {
+    return {
+        type: "posts/succeeded",
+        payload: posts
+    }
+}
+
+const fetchPostsFailed = (error) => {
+    return {
+        type: "posts/failed",
+        payload: error
+    }
+}
+
+// reducer
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "posts/requested":
+            return {
+                ...state,
+                loading: true,
+                error: ""
+            }
+
+        case "posts/succeeded":
+            return {
+                ...state,
+                loading: false,
+                posts: action.payload,
+                error: ""
+            }
+
+        case "posts/failed":
+            return {
+                ...state,
+                loading: false,
+                posts: [],
+                error: action.payload.message
+            }
+
+        default:
+            return state;
+    }
+}
+
+// thunk function
+const fetchPosts = () => {
+    return async (dispatch) => {
+        dispatch(fetchPostsRequested());
+
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
+            const posts = await response.json();
+
+            dispatch(fetchPostsSucceeded(posts));
+
+        } catch (err) {
+            dispatch(fetchPostsFailed(err));
+        }
+    }
+}
+
+// create store
+const store = createStore(reducer, applyMiddleware(thunkMiddleware.default));
+
+// subscribe to state changes
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// dispatch action
+store.dispatch(fetchPosts());
